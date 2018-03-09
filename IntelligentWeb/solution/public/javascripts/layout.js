cd69c6eee0602c16679bc548869e83f1d9d80f30
Will Garside <worgarside@gmail.com>
2018-03-09 13:11:30 +0000

    Added login form, needs styling though

---
+++ IntelligentWeb/solution/public/javascripts/layout.js
@@ -0,0 +1,29 @@
+console.log('Loaded layout.js');
+
+$(document).ready(function () {
+    var form = $('#login-form');
+    var btn = $('#btn-login');
+    var btn_left = btn.position()['left'];
+    var btn_width = btn.outerWidth(true);
+    var btn_right = btn_left + btn_width;
+    var form_width = form.outerWidth(true);
+
+    var form_right = $(document).width() - (btn_right + ((form_width - btn_width) / 2));
+
+    form.css({right: form_right});
+});
+
+$('#btn-login').on('click', function () {
+    $('#login-form').fadeToggle();
+
+});
+
+$(document).mouseup(function (e) {
+    var form = $('#login-form');
+    var btn = $('#btn-login');
+
+    // check mouse loc isn't form, button or descendant of form
+    if (!form.is(e.target) && !btn.is(e.target) && form.has(e.target).length === 0) {
+        form.fadeOut();
+    }
+});
