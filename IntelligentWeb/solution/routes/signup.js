8b246bb62be758dbaa7cd3f429aac42465ec00c1
Will Garside <worgarside@gmail.com>
2018-03-14 18:35:55 +0000

    Image uploading in place

--- IntelligentWeb/solution/routes/signup.js
+++ IntelligentWeb/solution/routes/signup.js
@@ -7,14 +7,14 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, './uploads');
+        callback(null, './public/images/uploads');
     },
     filename: function (req, file, callback) {
         callback(null, Date.now() + file.originalname);
     }
 });
 
-var upload = multer({storage: storage}).single('myFile');
+var upload = multer({storage: storage}).single('displayPicture');
 
 
 router.use(bodyParser.urlencoded({extended: true}));
@@ -29,69 +29,39 @@ var db;
 mongoClientObject.connect(url, function (err, client) {
     assert.equal(null, err);
 
-    if (err) return console.log(err);
-    if (!client) {
-        console.error("Error! Database connection failed.");
-        process.exit(1);
-    } else {
-        console.log("Connection established to", url);
-        db = client.db(dbName);
-    }
+    console.log("Connection established to", url);
+    db = client.db(dbName);
 });
 
-router.post('/add_user', function (req, res, next) {
 
-    console.log("\n\n#########################################");
-    console.log(req.body);
-    console.log("-----------------------------------------");
-    console.log(req.file);
-    console.log(req.files);
-    console.log("#########################################\n\n");
+router.post('/add_user', function (req, res) {
+    var userForename = req.body.forename;
+    var userSurname = req.body.surname;
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+    var userPhone = req.body.phone;
+    var userAge = req.body.age;
+    var userCounty = req.body.county;
 
     upload(req, res, function (err) {
-        if (err) {
-            return res.end("Error uploading");
-        }
-        // res.end("Success");
-        console.log("AMAZING WELL DONE");
+        if (err) throw err;
     });
 
+    var new_user = {
+        forename: userForename,
+        surname: userSurname,
+        email: userEmail,
+        password: userPassword,
+        phone: userPhone,
+        age: userAge,
+        county: userCounty,
+        privilege_level: 1
+    };
+
+    db.collection("users").insertOne(new_user, function (err, res) {
+        if (err) return console.log(err);
+    });
     res.redirect('/')
 });
-
-
-// router.post('/add_user', function (req, res) {
-//     var userForename = req.body.forename;
-//     var userSurname = req.body.surname;
-//     var userEmail = req.body.email;
-//     var userPassword = req.body.password;
-//     var userPhone = req.body.phone;
-//     var userAge = req.body.age;
-//     var userCounty = req.body.county;
-//
-//     console.log("\n\n#########################################");
-//     console.log(req.files);
-//     console.log("-----------------------------------------");
-//     console.log(req.file);
-//     console.log("#########################################\n\n");
-//
-//
-//
-//     var new_user = {
-//         forename: userForename,
-//         surname: userSurname,
-//         email: userEmail,
-//         password: userPassword,
-//         phone: userPhone,
-//         age: userAge,
-//         county: userCounty,
-//         privilege_level: 1
-//     };
-//
-//     db.collection("users").insertOne(new_user, function (err, res) {
-//         if (err) return console.log(err);
-//     });
-//     res.redirect('/')
-// });
 
 module.exports = router;
