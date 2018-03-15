fe39ebc22d4ddf480148baea3dd337cfd14cdb51
Rufus Cope <rcope2@sheffield.ac.uk>
2018-03-15 21:44:15 +0000

    restaurant add to DB almost mastered, still passing a lot of undefineds

--- IntelligentWeb/solution/routes/restaurant_new.js
+++ IntelligentWeb/solution/routes/restaurant_new.js
@@ -22,16 +22,34 @@ mongoClientObject.connect(url, function (err, client) {
 
 
 router.post('/add_restaurant', function (req, res) {
-    console.log('pppp');
     var resName = req.body.name;
     var resSuburb = req.body.suburb;
     var resAddress1 = req.body.address1;
     var resAddress2 = req.body.address2;
     var resCity = req.body.city;
-    var userAge = req.body.age;
-    var userCounty = req.body.county;
+    var resPostcode = req.body.postcode;
+    var resLatitude = 11111111111.2222; //CONNECT TO GOOGLE MAPS
+    var resLongitude = 22222222222.33333; //CONNECT TO GOOGLE MAPS
+    var resPrice_range = req.body.price_range;
+    var resDescription = req.body.description;
+    var resOpening_times = [0,0,0,0,0]; // SOME COMPLICATED MATHS
+    var resPhone = req.body.phone;
+    var resPublished = true;
+    var resDelivery = req.body.delivery;
+    var resTakeout = req.body.takeout;
+    var resParking = req.body.parking;
+    var resWifi = req.body.wifi;
+    var resOutdoor_seating = req.body.outdoor_seating;
+    var resReservations = req.body.reservations;
+    var resCategory = req.body.category;
+    var resImages = req.body.images;
+    var resAverage_rating = 1.0000000001;
+    var resMenu = req.body.menu;
+    var resURL = req.body.url;
+    var resAlcohol = req.body.wifi;
 
-    var new_user = {
+
+    var new_restaurant = {
         name: resName,
         suburb: resSuburb,
         address1: resAddress1,
@@ -52,15 +70,14 @@ router.post('/add_restaurant', function (req, res) {
         outdoor_seating: resOutdoor_seating,
         reservations: resReservations,
         category: resCategory,
-        reviews: resReviews,
         images: resImages,
-        owner_message: resOwner_message,
         average_rating: resAverage_rating,
         menu: resMenu,
         url: resURL,
-        alcohol: resAlcohol,
-        owner: resOwner
+        alcohol: resAlcohol
     };
+    console.log("this is a restaurant");
+    console.log(new_restaurant);
 
     db.collection("restaurants").insertOne(new_restaurant, function (err, res) {
         if (err) return console.log(err);
