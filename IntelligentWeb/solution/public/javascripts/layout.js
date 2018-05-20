3ddde10ae6ef3f40da6873f95ffed9d4f6f64ec8
Will Garside <worgarside@gmail.com>
2018-05-20 15:55:33 +0100

    Bugfix: scrolled floating login form handler

--- IntelligentWeb/solution/public/javascripts/layout.js
+++ IntelligentWeb/solution/public/javascripts/layout.js
@@ -17,8 +17,11 @@ $(() => {
 
 // ================================ Login Form ================================ \\
 
+const loginForm = $('#login-form');
+const loginBtn = $('#btn-login');
+const loginWarning = $('#login-warning');
 
-$('#btn-login').on('click', () => {
+loginBtn.on('click', () => {
     $('#login-form').fadeToggle();
 });
 
@@ -26,39 +29,39 @@ if (!userLoggedIn) {
 
     $(document)
         .mousedown((e) => {
-            const form = $('#login-form');
-            const btn = $('#btn-login');
-            const loginWarning = $('#login-warning');
+
 
             // check mouse loc isn't form, button or descendant of form
-            if (!form.is(e.target) && !btn.is(e.target) && !loginWarning.is(e.target) && form.has(e.target).length === 0) {
-                form.fadeOut();
+            if (!loginForm.is(e.target) && !loginBtn.is(e.target) && !loginWarning.is(e.target) && loginForm.has(e.target).length === 0) {
+                loginForm.fadeOut();
             }
 
         })
         .keydown((e) => {
             if (!userLoggedIn) {
                 if (e.keyCode === 27) { // escape key maps to keycode `27`
-                    $('#login-form').fadeOut();
+                    loginForm.fadeOut();
                 }
             }
         });
+
+    $(window).scroll(() => {
+        loginForm.fadeOut();
+    });
 }
 
 /**
  * Sets the location of the floating login form relative to the login button's location
  */
 function setLoginFormPosition() {
-    const form = $('#login-form');
-    const btn = $('#btn-login');
-    const btn_width = btn.outerWidth(true);
-    const btn_left = btn.position()['left'];
+    const btn_width = loginBtn.outerWidth(true);
+    const btn_left = loginBtn.position()['left'];
     const btn_right = btn_left + btn_width;
-    const form_width = form.outerWidth(true);
+    const form_width = loginForm.outerWidth(true);
 
     const form_right = $(document).width() - (btn_right + ((form_width - btn_width) / 2));
 
-    form.css({right: form_right});
+    loginForm.css({right: form_right});
 }
 
 // ================================ Input Formatting ================================ \\
